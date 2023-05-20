37b85492014e667b360385a759fe9d14ad7425df
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-20 16:21:26 +0600

    modified something

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ubvegtf.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
