7afe61deb94c3067ba491d24ba8663ad7de74b05
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-21 19:53:09 +0600

    service api and order api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,17 +22,36 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const servicesCollection = client.db('geniusCar').collection('services');
+        const ordersCollection = client.db('geniusCar').collection('oerders');
+
+        // services api:
+        app.get('/services', async (req, res) => {
+            const query = {} // to find all data or unspecific data
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        });
+
+        // order api: 
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        });
+
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
     }
 }
-run().catch(console.dir);
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello from Genius Car server.');
